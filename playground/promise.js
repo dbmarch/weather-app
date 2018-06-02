21263c827e34e8880b25b008207884e0585637ea
dbmarch <dbmarch@gmail.com>
2018-06-01 19:16:29 -0500

    promises

--- playground/promise.js
+++ playground/promise.js
@@ -1,8 +1,13 @@
 var somePromise = new Promise ((resolve, reject) => {
-    resolve ('Hey.  It worked!');
+    setTimeout(() => {
+         resolve ('Hey.  It worked!');
+         //reject('error');
+    }, 2500);
 });
 
 
 somePromise.then ( (message) => {
     console.log ('Success: ',  message);
+}, (errorMessage) => {
+    console.log ('Error: ', errorMessage);
 });
